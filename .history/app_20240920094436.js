4ad17ad0893a91cfa483e13d6d39c2f654f4a7c7
Kevin8548 <kevinarroyolopez231@gmail.com>
2024-09-25 13:06:49 -0600

    Metodos iteradores y high order functions

--- .history/app_20240920094436.js
+++
@@ -1,3 +0,0 @@
-// apuntar al div
-const ball = document.querySelector('div');
-const message = querySelector('h1');
--- .history/app_20240920094642.js
+++
@@ -1,7 +0,0 @@
-// apuntar al div
-const ball = document.querySelector('div');
-const message = querySelector('h1');
-
-const eightball = () => {
-    message.innerText = "Le diste click! waos"
-}
